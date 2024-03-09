0baf81758af84ba403e17b2c452267e5bd0b04e1
Sergei Solodskikh <ssolodskikh@adapty.io>
2024-03-09 20:21:59 +0100

    edits

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from recognition import recognize
+import threading
+import time
 
 import keyboard
-import threading
+import pyautogui
+import pyperclip
+from dotenv import load_dotenv
+
+from recognition import recognize
+
+load_dotenv()
+
+
+def recognize_callback(result):
+    # This function will be called with the result of `recognize`
+    pyperclip.copy(result)  # Copy result to clipboard
+    time.sleep(0.1)  # Short delay to ensure clipboard is updated
+    pyautogui.hotkey('command', 'v')  # Paste content
+
+
+def recognize_wrapper():
+    # This function wraps the call to `recognize` and handles its output
+    result = recognize()  # Assume this function returns the text to be pasted
+    recognize_callback(result)  # Pass the result to the callback function
 
 
 def on_hotkey_pressed():
     print("Hotkey pressed, activating...")
-    # Run your main function in a separate thread to avoid blocking
-    threading.Thread(target=recognize).start()
+    threading.Thread(target=recognize_wrapper).start()  # Use the wrapper function
 
 
 if __name__ == "__main__":
     # Set the hotkey to whatever you prefer
-    keyboard.add_hotkey('ctrl+alt', on_hotkey_pressed)  # Example: Pressing 'ctrl' and 'alt' together activates the recording
+
+    keyboard.add_hotkey('command+f1', on_hotkey_pressed)  # Example: Pressing 'ctrl' and 'alt' together activates the recording
 
     print("Listening for hotkey...")
+
     keyboard.wait('esc')  # Use 'esc' key to stop listening (or any key of your choice)
--- recognition.py
+++ recognition.py
@@ -2,6 +2,8 @@ from tempfile import NamedTemporaryFile
 
 import openai
 import os
+
+import pyperclip
 import speech_recognition as sr
 from pydub import AudioSegment
 from speech_recognition import AudioData
@@ -39,12 +41,13 @@ def transcribe_audio(file_path: str):
     return response.dict() if response else None
 
 
-def recognize():
+def recognize() -> str:
     # Capture audio from the default microphone
     with sr.Microphone() as source:
         print("Adjusting for ambient noise, please wait...")
         r.adjust_for_ambient_noise(source)  # Adjust for ambient noise
         print("Please start speaking...")
+        os.system('play --volume=0.1 -nq synth 0.2 sine 300')  # Play a sound to indicate recording has started
 
         try:
             # Listen for the first phrase and extract it into audio data
@@ -57,5 +60,7 @@ def recognize():
             # Transcribe the saved MP3 file
             transcription = transcribe_audio(mp3_file_path)
             print("Transcription: ", transcription)
+            return transcription['text'] if 'text' in transcription else ''
         except Exception as e:
             print(f"An error occurred: {e}")
+            return ''
